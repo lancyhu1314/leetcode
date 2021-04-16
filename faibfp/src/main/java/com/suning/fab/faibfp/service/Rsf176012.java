99766e8928532741f7390ba1ba3e42536bc91aa7
19043955 <19043955@cnsuning.com>
2021-04-16 17:53:51 +0800

    添加非跨库处理服务

---
+++ faibfp/src/main/java/com/suning/fab/faibfp/service/Rsf176012.java
@@ -0,0 +1,24 @@
+package com.suning.fab.faibfp.service;
+
+import com.suning.fab.faibfp.intf.FaloanMapService;
+import com.suning.fab.faibfp.service.template.RsfServiceTemplate;
+import com.suning.rsf.provider.annotation.Implement;
+import org.springframework.stereotype.Service;
+
+/**
+ * 功能描述: <br>
+ * 〈功能详细描述〉
+ *
+ * @Author 19043955
+ * @Date 2021/4/15
+ * @Version 1.0
+ */
+@Service
+@Implement(contract = FaloanMapService.class, implCode = "faloan-advanceAccountChargeDb")
+public class Rsf176012 extends RsfServiceTemplate {
+
+    @Override
+    protected String getTranCode() {
+        return "176012";
+    }
+}
