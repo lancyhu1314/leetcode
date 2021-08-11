af0ef2e5338c939bbf84f60fe3a91d4955713d38
19043955 <19043955@cnsuning.com>
2021-08-11 15:55:16 +0800

    mock测试

---
+++ faibfp/src/test/java/com/suning/fab/faibfp/localTest/Rsf477016Test.java
@@ -0,0 +1,36 @@
+package com.suning.fab.faibfp.localTest;
+
+import com.suning.fab.faibfp.service.Rsf477016;
+import com.suning.fab.faibfp.utils.TestUtil;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 功能描述: <br>
+ * 〈功能详细描述〉
+ *
+ * @Author 19043955
+ * @Date 2021/8/11
+ * @Version 1.0
+ */
+public class Rsf477016Test extends TestUtil {
+
+    @Autowired
+    Rsf477016 rsf477016;
+
+    @Test
+    public void test() {
+
+        Map<String, Object> input = new HashMap<String, Object>();
+        input.put("acctNo", "5103092300375594");
+        input.put("brc", "51030000");
+        input.put("tranCode", "477016");
+        input.put("termDate", "2021-08-04");
+
+        Map<String, Object> ret = rsf477016.execute(input);
+        System.out.println("=============" + ret);
+    }
+}
